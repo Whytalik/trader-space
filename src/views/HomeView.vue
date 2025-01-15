b0109f5d22755ca07aa4e35ba4ec522a60cb669f
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2025-01-15 22:09:24 +0200

    chore: set up routing, store, Tailwind, Prettier, and ESLint - Set up routing for navigation in the app. - Configured Pinia store for state management. - Integrated and configured Tailwind CSS for styling. - Set up Prettier for automatic code formatting. - Configured ESLint for code style enforcement and error detection.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-3xl">Home | Trader Space</div>
+  <div v-for="item in modelList" :key="item">
+    {{ item }}
+  </div>
+</template>
+
+<script setup>
+import { useModelStore } from "../store";
+
+const model = useModelStore();
+const modelList = model.modelList;
+</script>
