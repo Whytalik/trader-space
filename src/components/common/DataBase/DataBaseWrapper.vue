84cb3c35ed3bc67253b8489b26cdd0d61c72dce2
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2025-01-26 15:47:54 +0200

    feat: implemented database wrapper and added list & gallery view options
    - Created a wrapper component for managing and displaying database content.
    - Added ListView component for displaying data in a list format.
    - Added GalleryView component for displaying data in a grid/gallery format.
    - Implemented toggling between ListView and GalleryView.

---
+++ src/components/common/DataBase/DataBaseWrapper.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="database-wrapper">
+        <div class="database-header">
+            <h2 class="database-title">{{ title }}</h2>
+            <div class="database-actions">
+                <ViewToggle v-model="currentView" v-if="enableViewToggle" />
+                <slot name="actions"></slot>
+            </div>
+        </div>
+        <div class="database-content">
+            <ListView v-if="currentView === 'list'" :data="data">
+                <template #item-actions="slotProps">
+                    <slot name="item-actions" v-bind="slotProps"></slot>
+                </template>
+            </ListView>
+            <GalleryView v-else :data="data">
+                <template #item-actions="slotProps">
+                    <slot name="item-actions" v-bind="slotProps"></slot>
+                </template>
+            </GalleryView>
+        </div>
+    </div>
+</template>
+
+<script>
+import ViewToggle from './ViewToggle.vue'
+import ListView from './views/ListView.vue'
+import GalleryView from './views/GalleryView.vue'
+
+export default {
+    name: "DataBaseWrapper",
+    components: {
+        ViewToggle,
+        ListView,
+        GalleryView
+    },
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        data: {
+            type: Array,
+            required: true
+        },
+        enableViewToggle: {
+            type: Boolean,
+            default: true
+        },
+        defaultView: {
+            type: String,
+            default: 'list',
+            validator: (value) => ['list', 'gallery'].includes(value)
+        }
+    },
+    data() {
+        return {
+            currentView: this.defaultView
+        }
+    }
+}
+</script>
---
+++ src/components/common/DataBase/views/ListView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="list-view">
+    <div v-for="item in data" :key="item.id" class="list-item">
+      <div class="item-content">
+        <div class="item-header">
+          <h3 class="item-title">{{ item.title }}</h3>
+          <span class="item-date">{{ formatDate(item.date) }}</span>
+        </div>
+        <p class="item-description">{{ item.description }}</p>
+      </div>
+      <div class="item-actions">
+        <slot name="item-actions" :item="item"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListView",
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    formatDate(date) {
+      return new Date(date).toLocaleDateString()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.list-view {
+  @apply space-y-4;
+}
+
+.list-item {
+  @apply flex justify-between items-start p-4 bg-background 
+         border border-input-border rounded-lg hover:border-input-focus transition-all duration-fast;
+}
+
+.item-content {
+  @apply flex-1;
+}
+
+.item-header {
+  @apply flex justify-between items-center mb-2;
+}
+
+.item-title {
+  @apply text-lg font-medium;
+}
+
+.item-date {
+  @apply text-sm text-gray-500;
+}
+
+.item-description {
+  @apply text-sm text-gray-600 dark:text-gray-400;
+}
+
+.item-actions {
+  @apply flex gap-2 ml-4;
+}
+</style> 
