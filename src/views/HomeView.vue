3b6477ccbfbeb9b72a268ab97bc1ece67d7eb19b
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2025-02-02 21:36:23 +0200

    feat: added profit chart display
    - Implemented a chart to display total profit per trade
    - Used Chart.js to visualize data in a chart

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,19 +10,24 @@
       <TodayTradesWrapper />
       <TodayRoutinesWrapper />
     </div>
+
+    <ProfitLineChart :trades="trades" />
   </div>
 </template>
 
 <script setup>
 import { computed } from "vue";
 import { useUserStore } from "@/stores/user";
+import { useTradesStore } from "@/stores/trades";
 import TodayTradesWrapper from "@/components/dashboard/TodayTradesWrapper.vue";
 import TodayRoutinesWrapper from "@/components/dashboard/TodayRoutinesWrapper.vue";
 import InfoBar from "@/components/dashboard/InfoBar.vue";
 import GreetingComponent from "../components/dashboard/GreetingComponent.vue";
-
+import ProfitLineChart from "@/components/charts/ProfitLineChart.vue";
 const userStore = useUserStore();
 const username = computed(() => userStore.currentUser.username);
+const tradesStore = useTradesStore();
+const trades = computed(() => tradesStore.trades);
 </script>
 
 <style scoped></style>
